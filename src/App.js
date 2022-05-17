7ee6726732fb024ed81d28f48e31c2c1d0d6b3a1
lekshmi12345 <lekshmisnair213@gmail>
2022-05-17 13:46:12 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Addbooks from './component/Addbooks';
+import Header from './component/Header';
 function App() {
   return (
     <>
+    <Header/>
     <Addbooks/>
     
     </>
