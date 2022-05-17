b76bd9bc209ed798a33e00fa55456a93728dd518
lekshmi12345 <lekshmisnair213@gmail>
2022-05-17 12:41:13 +0530

    search updated

--- src/component/Searchbook.js
+++ src/component/Searchbook.js
@@ -1,10 +1,15 @@
+import React from 'react'
+
+const Searchbook = () => {
+  return (
+    <div> 
 <div class="container">
     <div class="row">
         <div class="col col-12 div-sm-12 col-md-12 col-lg-12 div-xl-12 col-xxl-12">
-            <div class="row">
+            <div class="row g-3">
                 <div class="col col-12 div-sm-12 col-md-12 col-lg-12 div-xl-12 col-xxl-12 ">
                     <label for="" class="form-label">Book title</label>
-                    <input type="text" class="form-control">
+                    <input type="text" class="form-control"/>
                 </div>
                 
                 <div class="col col-12 div-sm-12 col-md-12 col-lg-12 div-xl-12 col-xxl-12">
@@ -14,3 +19,8 @@
         </div>
     </div>
 </div>
+    </div>
+  )
+}
+
+export default Searchbook
